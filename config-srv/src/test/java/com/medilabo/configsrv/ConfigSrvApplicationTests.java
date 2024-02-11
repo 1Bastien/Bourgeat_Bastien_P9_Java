64d32dfd7f55673a8f7d0f14307e05fcb60036c1
Bastien <bastien.brgt@protonmail.com>
2024-02-11 11:52:33 +0100

    feat(config-srv): initialize config-srv project

---
+++ config-srv/src/test/java/com/medilabo/configsrv/ConfigSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.configsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
