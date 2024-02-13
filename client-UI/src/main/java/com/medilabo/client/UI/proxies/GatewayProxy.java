d0f09fa5e278ae12b6ce261ad79271affd66356a
Bastien <bastien.brgt@protonmail.com>
2024-02-13 14:53:38 +0100

    feat(client-ui): Add Feign proxy and PatientDto

---
+++ client-UI/src/main/java/com/medilabo/client/UI/proxies/GatewayProxy.java
@@ -0,0 +1,36 @@
+package com.medilabo.client.UI.proxies;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.medilabo.client.UI.Dto.PatientDto;
+import com.medilabo.client.UI.configuration.GatewayFeignConfiguration;
+
+@FeignClient(name = "gateway", configuration = GatewayFeignConfiguration.class)
+public interface GatewayProxy {
+
+	@PostMapping("/patient-api/patient")
+	PatientDto addPatient(PatientDto patient, @RequestHeader("Authorization") String authorizationHeader);
+
+	@GetMapping("/patient-api/patient/all")
+	List<PatientDto> getPatients(@RequestHeader("Authorization") String authorizationHeader);
+
+	@GetMapping("/patient-api/patient/{id}")
+	PatientDto getPatient(@PathVariable("id") Long id, @RequestHeader("Authorization") String authorizationHeader);
+
+	@PostMapping("/patient-api/patient/update/{id}")
+	PatientDto updatePatient(@PathVariable("id") Long id, PatientDto patient,
+			@RequestHeader("Authorization") String authorizationHeader);
+
+	@GetMapping("/patient-api/patient/delete/{id}")
+	void deletePatient(@PathVariable("id") Long id, @RequestHeader("Authorization") String authorizationHeader);
+
+	@GetMapping("/login")
+	ResponseEntity<String> login(@RequestHeader("Authorization") String authorizationHeader);
+}
