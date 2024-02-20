f4648d257482875b0c82e6bba4b2da5a0020065c
Bastien <bastien.brgt@protonmail.com>
2024-02-20 18:22:51 +0100

    feat(assessment-service): Add AssessmentController and its tests

---
+++ assessment-service/src/main/java/com/medilabo/assessmentservice/controller/AssessmentController.java
@@ -0,0 +1,26 @@
+package com.medilabo.assessmentservice.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medilabo.assessmentservice.Dto.AssessmentType;
+import com.medilabo.assessmentservice.services.AssessmentService;
+
+@RestController
+public class AssessmentController {
+
+	private static final Logger logger = LogManager.getLogger(AssessmentController.class);
+
+	@Autowired
+	private AssessmentService assessmentService;
+
+	@GetMapping("/assessment/{patientId}")
+	public AssessmentType getAssessment(@PathVariable("patientId") Long patientId) {
+		logger.info("GET /assessment/" + patientId);
+		return assessmentService.getAssessment(patientId);
+	}
+}
---
+++ assessment-service/src/test/java/com/medilabo/assessmentservice/controller/AssessmentControllerTest.java
@@ -0,0 +1,36 @@
+package com.medilabo.assessmentservice.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.medilabo.assessmentservice.Dto.AssessmentType;
+import com.medilabo.assessmentservice.services.AssessmentService;
+
+@WebMvcTest(AssessmentController.class)
+public class AssessmentControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private AssessmentService assessmentService;
+
+	@Test
+	public void testGetAssessment() throws Exception {
+		when(assessmentService.getAssessment(1L)).thenReturn(any(AssessmentType.class));
+
+		mockMvc.perform(get("/assessment/1")).andExpect(status().isOk());
+
+		verify(assessmentService, times(1)).getAssessment(1L);
+	}
+}
