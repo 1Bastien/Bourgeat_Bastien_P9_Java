80ca7862142adaff657f4310e053d688019c68aa
Bastien <bastien.brgt@protonmail.com>
2024-02-22 16:13:59 +0100

    refactor(assessment-service): Moved FeedbackProxy URLs in conf. file

--- assessment-service/src/main/java/com/medilabo/assessmentservice/proxies/impl/FeedbackProxyImpl.java
+++ assessment-service/src/main/java/com/medilabo/assessmentservice/proxies/impl/FeedbackProxyImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,10 +23,13 @@ public class FeedbackProxyImpl implements FeedbackProxy {
 	@Autowired
 	private WebClient.Builder webClient;
 	
+	@Value("${feedback.service.url}")
+	private String feedbackServiceUrl;
+	
 	@Override
 	public int countWordOccurrences(Long patientId, List<String> keywords) {
 		try {
-			URI uri = UriComponentsBuilder.fromUriString("http://feedback-service/feedback/patient/assessment/{patientId}")
+			URI uri = UriComponentsBuilder.fromUriString(feedbackServiceUrl + "{patientId}")
 					.queryParam("keywords", keywords).buildAndExpand(patientId).toUri();
 			
 			int count = webClient.build().get().uri(uri).retrieve().bodyToMono(Integer.class).block();
